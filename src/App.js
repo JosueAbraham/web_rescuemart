4ac4aae401bde43d0e1b044e18adb971c509d6a6
JosueAbraham <rodriguez.razo.josue@gmail.com>
2023-11-07 11:22:15 -0600

    Cambios de archivos
    Cambios de archivos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import styled from 'styled-components';
+import Header from './Header';
+import Home from './Home';
+import Catalogo from './Catalogo';
+import DetalleProducto from './DetalleProducto';
+import Footer from './Footer';
 
-function App() {
+
+
+const AppContainer = styled.div`
+  font-family: Arial, sans-serif;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh; /* Establece el alto mínimo para todo el viewport */
+`;
+
+const Content = styled.main`
+  flex: 1; /* Permite que el contenido principal ocupe el espacio disponible */
+`;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <AppContainer>
+        <Header />
+        <Content>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Catalogo" element={<Catalogo />} />
+            <Route path="/productos/:id" element={<DetalleProducto />} />
+          </Routes>
+        </Content>
+        <Footer />
+      </AppContainer>
+    </Router>
   );
 }
 
