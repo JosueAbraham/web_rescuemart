3769aa78d29930ce5e3324e3e55b11c97a00f9d8
Nicolás A. Flores <116039096+nikitoonsbug@users.noreply.github.com>
2023-11-22 11:48:29 -0600

    Paypal button

--- src/Paypal_Button.js
+++ src/Paypal_Button.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { PayPalScriptProvider, PayPalButtons } from '@paypal/react-paypal-js';
+import { toast as toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 class PaypalButton extends React.Component {
   render() {
@@ -22,13 +26,20 @@ class PaypalButton extends React.Component {
               return actions.order.capture().then(function (details) {
                 // Maneja el pago exitoso aquí
                 console.log(details);
+                // Muestra un mensaje de compra exitosa
+                toast.success('Compra hecha exitosamente', { position: toast.POSITION.TOP_CENTER });
+              }).catch(function (error) {
+                // Maneja el error aquí
+                console.log(error);
+                // Muestra un mensaje de error
+                toast.error('Error al procesar el pago', { position: toast.POSITION.BOTTOM_RIGHT });
               });
             }}
+            
           />
         </div>
       </PayPalScriptProvider>
     );
   }
 }
-
 export default PaypalButton;
