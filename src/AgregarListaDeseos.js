23ecafeee6273ac9c701cd461c8245fb01b6df63
JosueAbraham <rodriguez.razo.josue@gmail.com>
2023-12-03 14:21:10 -0600

    Se agregaron 2 nuevos componentes,
    Se agregaron 2 nuevos componentes,

---
+++ src/AgregarListaDeseos.js
@@ -0,0 +1,100 @@
+// AgregarListaDeseos.js
+import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import styled from 'styled-components';
+
+const ButtonListaDeseos = styled.button`
+  margin: 10px auto;
+  background-color: ${({ isInListaDeseos }) => (isInListaDeseos ? '#ff3333' : '#ffcc00')};
+  color: #fff;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 6px;
+  font-size: 18px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${({ isInListaDeseos }) => (isInListaDeseos ? '#e60000' : '#ffdb4d')};
+  }
+`;
+
+const AgregadoExitosamente = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  background-color: #333333;
+  color: #fff;
+  text-align: center;
+  padding: 15px;
+  font-size: 18px;
+  z-index: 1000;
+  opacity: ${({ show }) => (show ? 1 : 0)};
+  transform: translateY(${({ show }) => (show ? '0' : '-100%')});
+  transition: opacity 0.3s ease, transform 0.3s ease;
+`;
+
+const MensajeEliminado = styled(AgregadoExitosamente)`
+  background-color: #ff3333; // Fondo rojo para mensaje de eliminación
+`;
+
+const AgregarListaDeseos = ({ selectedProduct }) => {
+    const [listaDeseos, setListaDeseos] = useState([]);
+    const [mensajeAgregado, setMensajeAgregado] = useState(false);
+    const [productoAgregado, setProductoAgregado] = useState(false);
+
+    useEffect(() => {
+        const storedListaDeseos = localStorage.getItem('listaDeseos');
+        if (storedListaDeseos) {
+            setListaDeseos(JSON.parse(storedListaDeseos));
+        }
+    }, []);
+
+    const isInListaDeseos = listaDeseos.some(producto => producto.id === selectedProduct.id);
+
+    const handleAgregarEliminarListaDeseos = () => {
+        if (isInListaDeseos) {
+            // El producto ya está en la lista, así que lo eliminamos
+            const nuevaListaDeseos = listaDeseos.filter(producto => producto.id !== selectedProduct.id);
+            setListaDeseos(nuevaListaDeseos);
+            localStorage.setItem('listaDeseos', JSON.stringify(nuevaListaDeseos));
+            setProductoAgregado(false); // Se ha eliminado un producto
+        } else {
+            // El producto no está en la lista, así que lo agregamos
+            const nuevaListaDeseos = [...listaDeseos, selectedProduct];
+            setListaDeseos(nuevaListaDeseos);
+            localStorage.setItem('listaDeseos', JSON.stringify(nuevaListaDeseos));
+            setProductoAgregado(true); // Se ha agregado un producto
+            setMensajeAgregado(true);
+            setTimeout(() => {
+                setMensajeAgregado(false);
+            }, 3000);
+        }
+    };
+
+    return (
+        <>
+            <ButtonListaDeseos onClick={handleAgregarEliminarListaDeseos} isInListaDeseos={isInListaDeseos}>
+                <FontAwesomeIcon icon={faHeart} /> {isInListaDeseos ? 'Eliminar de Lista de Deseos' : 'Agregar a Lista de Deseos'}
+            </ButtonListaDeseos>
+            {mensajeAgregado && (
+                <>
+                    {productoAgregado ? (
+                        <AgregadoExitosamente show={mensajeAgregado}>
+                            <FontAwesomeIcon icon={faHeart} /> Producto agregado a la Lista de Deseos exitosamente
+                        </AgregadoExitosamente>
+                    ) : (
+                        <MensajeEliminado show={mensajeAgregado}>
+                            <FontAwesomeIcon icon={faHeart} /> Producto eliminado de la Lista de Deseos exitosamente
+                        </MensajeEliminado>
+                    )}
+                </>
+            )}
+        </>
+    );
+};
+
+export default AgregarListaDeseos;
+
+
---
+++ src/ListaDeseosComponent.js
@@ -0,0 +1,163 @@
+// ListaDeseosComponent.js
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart, faTrash } from '@fortawesome/free-solid-svg-icons';
+import AgregarCarrito from './AgregarCarrito';
+
+const ListaDeseosContainer = styled.div`
+  max-width: 1000px;
+  margin: 20px auto;
+  padding: 20px;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+`;
+
+const ListaDeseosList = styled.ul`
+  list-style: none;
+  padding: 0;
+`;
+
+const ListaDeseosItem = styled.li`
+  margin-bottom: 20px;
+  border-bottom: 1px solid #ddd;
+  padding-bottom: 10px;
+`;
+
+const ItemInfo = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const ItemImage = styled.img`
+  max-width: 150px;
+  margin-right: 10px;
+`;
+
+const ItemDetails = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  font-size: 16px;
+`;
+
+const StyledButtonDelete = styled.button`
+  margin: 10px auto;
+  background-color: #dd2a62;
+  color: white;
+  padding: 12px 10px;
+  border: none;
+  border-radius: 6px;
+  font-size: 18px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #ff6630;
+  }
+`;
+
+const EmptyListaDeseosMessage = styled.p`
+  font-size: 18px;
+  text-align: center;
+  color: #555;
+  margin-bottom: 20%;
+`;
+
+const ListaDeseosComponent = () => {
+    const [listaDeseos, setListaDeseos] = useState([]);
+    const [setProductosDestacados] = useState([]);
+
+    useEffect(() => {
+        const fetchProductosDestacados = async () => {
+            try {
+                const response = await fetch(process.env.PUBLIC_URL + '/products.json');
+                if (!response.ok) {
+                    throw new Error('Error al cargar productos destacados');
+                }
+                const data = await response.json();
+                const favoritos = data.filter(producto => producto.favorito);
+
+                // Obtén la lista de deseos actual del almacenamiento local
+                const storedListaDeseos = localStorage.getItem('listaDeseos');
+                const listaDeseosActual = storedListaDeseos ? JSON.parse(storedListaDeseos) : [];
+
+                // Verifica si cada producto favorito ya está en la lista de deseos antes de agregarlo
+                const nuevosFavoritos = favoritos.filter(favorito => (
+                    !listaDeseosActual.some(producto => producto.id === favorito.id)
+                ));
+
+                // Combina los nuevos productos favoritos con la lista de deseos actual
+                const nuevaListaDeseos = [...listaDeseosActual, ...nuevosFavoritos];
+                setListaDeseos(nuevaListaDeseos);
+                localStorage.setItem('listaDeseos', JSON.stringify(nuevaListaDeseos));
+
+                setProductosDestacados(nuevosFavoritos);
+            } catch (error) {
+                console.error('Error al cargar productos destacados:', error.message);
+            }
+        };
+
+        fetchProductosDestacados();
+    }, []);
+
+
+    useEffect(() => {
+        const storedListaDeseos = localStorage.getItem('listaDeseos');
+        if (storedListaDeseos) {
+            setListaDeseos(JSON.parse(storedListaDeseos));
+        }
+    }, []);
+
+    const handleEliminarProducto = (index) => {
+        const nuevaListaDeseos = [...listaDeseos];
+        nuevaListaDeseos.splice(index, 1);
+        setListaDeseos(nuevaListaDeseos);
+        localStorage.setItem('listaDeseos', JSON.stringify(nuevaListaDeseos));
+    };
+
+    const handleVaciarListaDeseos = () => {
+        setListaDeseos([]);
+        localStorage.removeItem('listaDeseos');
+    };
+
+    return (
+        <ListaDeseosContainer>
+            <h1><FontAwesomeIcon icon={faHeart} /> Lista de Deseos</h1>
+            {listaDeseos.length > 0 ? (
+                <ListaDeseosList>
+                    {listaDeseos.map((item, index) => (
+                        <ListaDeseosItem key={index}>
+                            <ItemInfo>
+                                <ItemImage
+                                    src={process.env.PUBLIC_URL + '/' + item.imagen}
+                                    alt={item.nombre}
+                                />
+                                <ItemDetails>
+                                    <strong>{item.nombre}</strong>
+                                    <p><strong>Precio: </strong>${item.precio.toFixed(2)}</p>
+                                </ItemDetails>
+                                <StyledButtonDelete onClick={() => handleEliminarProducto(index)}>
+                                    <FontAwesomeIcon icon={faTrash} />
+                                </StyledButtonDelete>
+                                <AgregarCarrito selectedProduct={item} cantidad={1} />
+                            </ItemInfo>
+                        </ListaDeseosItem>
+                    ))}
+                </ListaDeseosList>
+            ) : (
+                <EmptyListaDeseosMessage>¡Ups! Parece que tu lista de deseos está vacía. ¿Por qué no agregas algunos productos?</EmptyListaDeseosMessage>
+            )}
+            {listaDeseos.length > 0 && (
+                <div>
+                    <StyledButtonDelete onClick={handleVaciarListaDeseos}>
+                        <FontAwesomeIcon icon={faTrash} /> Vaciar Lista de Deseos
+                    </StyledButtonDelete>
+                </div>
+            )}
+        </ListaDeseosContainer>
+    );
+};
+
+export default ListaDeseosComponent;
